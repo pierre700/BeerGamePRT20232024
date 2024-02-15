3cef9cc4569357ddb9f89d8db9e70a6df2a4db4e
pierre700 <pierre.renault70@gmail.com>
2024-02-15 10:30:05 +0100

    first commit

---
+++ client/src/pages/PlayGame.js
@@ -0,0 +1,345 @@
+import "../styles/pages/PlayGame.css"
+import InputField from "../components/form/InputField"
+import {useEffect, useState} from "react";
+import Button from "../components/form/Button";
+
+function PlayGame(props) {
+
+    const gameCode = JSON.parse(localStorage.getItem("room")) //Code du jeu
+    const selectedRole = JSON.parse(localStorage.getItem("role")) //Rôle de jeu choisi
+    // ==> 1: Producteur | 2: Distributeur | 3: Grossiste | 4: Détaillant | 5: Maitre du Jeu
+
+    const socket = props.socketId
+
+    const [orderValue, setOrderValue] = useState("") //Commande
+    const [producerDelay, setProducerDelay] = useState("")
+    const [distributorDelay, setDistributorDelay] = useState("")
+    const [wholesalerDelay, setWholesalerDelay] = useState("")
+    const [retailerDelay, setRetailerDelay] = useState("")
+    const [inputActive, setInputActive] = useState(true) //Active ou désactive le champ de saisie de la commande
+    const [currentRoomSize, setCurrentRoomSize] = useState(0) //Joueurs actuels dans le jeu
+    const [currentRoomRoles, setCurrentRoomRoles] = useState([]) //Rôles déjà occupés
+
+    const [gameRounds, setGameRounds] = useState(0) //Tours de jeu (total)
+    const [currentRound, setCurrentRound] = useState(0) //Tour de jeu actuel
+    const [stock, setStock] = useState(0) //Inventaire
+    const [retard, setRetard] = useState(0) //Retard
+    const [next1WeekDelivery, setNext1WeekDelivery] = useState(0) //Livraison la semaine prochaine
+    const [next2WeekDelivery, setNext2WeekDelivery] = useState(0) //Livraison la semaine encore d'après
+    const [supplyChainOrder, setSupplyChainOrder] = useState(0) //Demande de livraison
+
+    const[dataV, setDataV] = useState(0)    
+
+    useEffect(() => {
+      // Update_Player_Data: Appelé quand tout le monde a passé la commande.
+      // Les données sont calculées par le serveur puis transmises aux clients
+        socket.on("update_player_data", (data) => {
+            setDataV(data)
+            console.log("Appele UpdatePlayer")
+            console.log(data)
+            setCurrentRound(data.roundData.currentRound)
+            console.log(data.roundData.currentRound)
+            setInputActive(true)
+            if(selectedRole === 1) {
+                setStock(data.roundData.producer[data.roundData.currentRound-1].stock)
+                setRetard(data.roundData.producer[data.roundData.currentRound-1].retard)
+                setNext1WeekDelivery(data.roundData.producer[data.roundData.currentRound-1].next1Week)
+                setNext2WeekDelivery(data.roundData.producer[data.roundData.currentRound-1].next2Week)
+                setSupplyChainOrder(data.roundData.distributor[data.roundData.currentRound-1].order)
+            }
+            else if(selectedRole === 2) {
+                setStock(data.roundData.distributor[data.roundData.currentRound-1].stock)
+                setRetard(data.roundData.distributor[data.roundData.currentRound-1].retard)
+                setNext1WeekDelivery(data.roundData.distributor[data.roundData.currentRound-1].next1Week)
+                setNext2WeekDelivery(data.roundData.distributor[data.roundData.currentRound-1].next2Week)
+                setSupplyChainOrder(data.roundData.wholesaler[data.roundData.currentRound-1].order)
+            }
+            else if(selectedRole === 3) {
+                setStock(data.roundData.wholesaler[data.roundData.currentRound-1].stock)
+                setRetard(data.roundData.wholesaler[data.roundData.currentRound-1].retard)
+                setNext1WeekDelivery(data.roundData.wholesaler[data.roundData.currentRound-1].next1Week)
+                setNext2WeekDelivery(data.roundData.wholesaler[data.roundData.currentRound-1].next2Week)
+                setSupplyChainOrder(data.roundData.retailer[data.roundData.currentRound-1].order)
+            }
+            else {
+                setStock(data.roundData.retailer[data.roundData.currentRound-1].stock)
+                setRetard(data.roundData.retailer[data.roundData.currentRound-1].retard)
+                setNext1WeekDelivery(data.roundData.retailer[data.roundData.currentRound-1].next1Week)
+                setNext2WeekDelivery(data.roundData.retailer[data.roundData.currentRound-1].next2Week)
+                
+                setSupplyChainOrder(data.roundData.demandClient)
+                }
+
+            
+        })
+        socket.on("initial_data", (data) => {
+            console.log(data)
+            setGameRounds(data.gameSettings.rounds)
+            setStock(data.gameSettings.startStock)
+        })
+        socket.on("update_room_size", (data) => {
+            setCurrentRoomSize(data.roomSize)
+            setCurrentRoomRoles(data.selectedRoles)
+        })
+    })
+
+    function submitOrder() {
+        setInputActive(false)
+        socket.emit("game_update", {
+            gameCode,
+            selectedRole,
+            orderValue
+        })
+        setOrderValue("")
+    }
+
+    function submitDelay() {
+        setInputActive(false)
+        socket.emit("game_update", {
+            gameCode, 
+            selectedRole,
+            producerDelay,
+            distributorDelay,
+            wholesalerDelay,
+            retailerDelay
+        })
+        setProducerDelay("")
+        setDistributorDelay("")
+        setWholesalerDelay("")
+        setRetailerDelay("")
+    }
+
+    if(currentRoomSize < 5) {
+        return (
+            <div>
+                <div className={"grid_play"}>
+                    <div className={"playground"}>
+                        <h2>En attente de coéquipiers</h2>
+                        <p>Sont en ce moment <b>{ currentRoomSize }</b> sur <b>5</b> joueurs dans le hall.</p>
+                        <p>======================== Les rôles suivants sont occupés ========================</p>
+                        { currentRoomRoles.map(element => {
+                            return <p key={element}>{element}</p>
+                        }) }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else {
+        if(currentRound<=gameRounds){
+        let orderInputAndButton = <></>
+        let delayInputAndButton = <></>
+        if(inputActive) {
+            delayInputAndButton = (
+                <>
+                    <InputField
+                        name={"producerDelay"}
+                        getValue={setProducerDelay}
+                        setValue={producerDelay}
+                        description={"Allowed Characters: 0-9"}
+                    />
+                    <InputField
+                        name={"distributorDelay"}
+                        getValue={setDistributorDelay}
+                        setValue={distributorDelay}
+                        description={"Allowed Characters: 0-9"}
+                    />
+                    <InputField
+                        name={"wholesalerDelay"}
+                        getValue={setWholesalerDelay}
+                        setValue={wholesalerDelay}
+                        description={"Allowed Characters: 0-9"}
+                    />
+                    <InputField
+                        name={"retailerDelay"}
+                        getValue={setRetailerDelay}
+                        setValue={retailerDelay}
+                        description={"Allowed Characters: 0-9"}
+                    />
+                    <Button onClick={submitDelay}>Commande</Button>
+                </>
+            )
+            orderInputAndButton = (
+                <>
+                    <InputField
+                        name={"OrderQuantity"}
+                        getValue={setOrderValue}
+                        setValue={orderValue}
+                        description={"Allowed Characters: 0-9"}
+                    />
+                    <Button onClick={submitOrder}>Commande</Button>
+                </>
+            )
+        }
+        else {
+            delayInputAndButton = (
+                <>
+                    <InputField
+                        name={"producerDelay"}
+                        getValue={setProducerDelay}
+                        setValue={producerDelay}
+                        description={"Allowed Characters: 0-9"}
+                        disabled={true}
+                    />
+                    <InputField
+                        name={"distributorDelay"}
+                        getValue={setDistributorDelay}
+                        setValue={distributorDelay}
+                        description={"Allowed Characters: 0-9"}
+                        disabled={true}
+                    />
+                    <InputField
+                        name={"wholesalerDelay"}
+                        getValue={setWholesalerDelay}
+                        setValue={wholesalerDelay}
+                        description={"Allowed Characters: 0-9"}
+                        disabled={true}
+                    />
+                    <InputField
+                        name={"retailerDelay"}
+                        getValue={setRetailerDelay}
+                        setValue={retailerDelay}
+                        description={"Allowed Characters: 0-9"}
+                        disabled={true}
+                    />
+                    <Button onClick={submitDelay}>Commande</Button>
+                </>
+            )
+            orderInputAndButton = (
+                <>
+                    <InputField
+                        name={"OrderQuantity"}
+                        getValue={setOrderValue}
+                        setValue={orderValue}
+                        description={"Caractères autorisés: 0-9"}
+                        disabled={true}
+                    />
+                    <Button onClick={submitOrder}>Commande</Button>
+                </>
+            )
+        }
+
+        let roleIcon = <></>
+        let roleName = ""
+        if(selectedRole === 1) {
+            roleIcon = "/icons/factory.svg"
+            roleName = "Producteur"
+        }
+        else if(selectedRole === 2) {
+            roleIcon = "/icons/box.svg"
+            roleName = "Distributeur"
+        }
+        else if(selectedRole === 3) {
+            roleIcon = "/icons/wholesale.svg"
+            roleName = "Grossiste"
+        }
+        else if(selectedRole === 4) {
+            roleIcon = "/icons/shop.svg"
+            roleName = "Détaillant"
+        }
+        else if(selectedRole === 5) {
+            roleIcon = "/icons/GM.svg"
+            roleName = "maitre du jeu"
+        }
+
+        function endGameBtn()
+        {
+            console.log("End Game")
+            setCurrentRound(gameRounds+1)
+        }
+        if(selectedRole === 5){
+            return (
+                <div>
+                  <div className={"grid_play"}>
+                    <div className={"playground"}>
+                      <div className={"timer"}>
+                        <p>Tour {currentRound}/{gameRounds}</p>
+                      </div>
+                      {/* ... other elements */}
+                      <div className={"line"} />
+                      <div className={"new_order"}>
+                        {/* Input fields for player delays */}
+                        <div>
+                            <span>Nouveaux délais :</span>
+                            { delayInputAndButton }
+                        </div>
+                      </div>
+                      <div className={"line"} />
+                      {/* ... other elements */}
+                    </div>
+                  </div>
+                </div>
+              );
+        }
+        else {
+            return (
+                    <div>
+                        <div className={"grid_play"}>
+                            <div className={"playground"}>
+                                <div className={"timer"}>
+                                    <p>Tour {currentRound}/{gameRounds}</p>
+                                </div>
+                                <div className={"wrapper_img"}>
+                                    <img src={roleIcon} alt={"Icon"} />
+                                    <span>{roleName}</span>
+                                </div>
+                                <div className={"line"} />
+                                <div className={"wrapper_1"}>
+                                    <span>Stock : { stock }</span>
+                                    <span>Retard : { retard }</span>
+                                </div>
+                                <div className={"line"} />
+                                <div className={"new_order"}>
+                                    <span>Nouvel ordre :</span>
+                                    { orderInputAndButton }
+                                </div>
+                                <div className={"line"} />
+                                <>
+                                    <span>Livraison :</span>
+                                    <div className={"next_products"}>
+                                        <span>La semaine prochaine : {next1WeekDelivery}</span>
+                                        <span>La semaine après : {next2WeekDelivery}</span>
+                                    </div>
+                                </>
+                                <div className={"line"} />
+                                <div className={"delivery"}>
+                                    <span>Demande : {supplyChainOrder}</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    <div>&nbsp;</div>
+
+                    <div className={"grid_play2"}>
+                        <div className={"playground2"}>
+                            <div className={"KPItable"}>
+                            <Button onClick={endGameBtn}>Finir la partie</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+    
+    else{
+        function exportData(){
+            console.log("Exporting data")
+            socket.emit("endGame",{dataV})
+        }
+        return (
+        <div>
+        <span>La partie est finie !</span>
+        <br />
+        <Button onClick={exportData}>Exporter les données</Button>
+        <br />
+        <span>(Le fichier sera disponible sur le serveur hébergeur, dans le dossier "server", sous le nom de "export.xlsx")</span>
+        </div>
+        )
+    }
+    
+}
+
+}
+
+export default PlayGame
---
+++ server/functions/CalculateNewValues.js
@@ -0,0 +1,72 @@
+export default function CalculateNewValues(RoleID, role, NextRole, currentRound, buffer, dmdCli) {
+  let
+    newStock = role[currentRound].stock,
+    newNextStock = NextRole.stock,
+    newRetard = role[currentRound].retard,
+    newNextRetard = NextRole.retard,
+    currentOrder = 0,
+    deliveryValueToNextRole = 0;
+
+  for (var i = 0; i < buffer.length; i++){
+    console.log("boucle1", currentRound, buffer[i][1], buffer[i][0])
+    if (currentRound === buffer[i][0]){
+      currentOrder = buffer[i][1]
+      console.log("order", currentOrder)
+    }
+  }
+
+  //If Stock >= Order
+  if (RoleID === 2 | RoleID === 3 | RoleID === 4){
+    console.log("if1", newNextStock, currentOrder)
+    if(newNextStock >= currentOrder) {
+      console.log("if2")
+      newStock = newStock + currentOrder
+      newNextStock= newNextStock - currentOrder
+      if(newNextRetard > 0) {
+        console.log("if3")
+        //If Retard - Inventory >= 0
+        if((newNextRetard - newNextStock) >= 0) {
+          console.log("if4")
+          newNextRetard = newNextRetard - newNextStock
+          newNextStock = 0
+        }
+        else {
+          console.log("else4")
+          newNextStock = newNextStock - newNextRetard
+          newStock = newStock + newNextRetard
+          newNextRetard = 0
+        }
+      }
+    }
+    else {
+      console.log("else2")
+      const diff = currentOrder - newNextStock
+      newStock = newStock + newNextStock
+      newNextStock = 0
+      newNextRetard = newNextRetard + diff
+      
+    }
+    if (RoleID === 4){
+      console.log("if5")
+      if (newStock >= dmdCli){
+        console.log("if6")
+        newStock = newStock - dmdCli
+      }
+      else {
+        console.log("else6")
+        newRetard = newRetard - dmdCli + newStock
+        newStock = 0
+      }
+    }
+  }
+  else {
+    console.log("else1", currentOrder)
+    newStock = newStock + currentOrder
+  }
+
+  role[currentRound].stock = newStock
+  NextRole.retard = newNextRetard
+  NextRole.stock = newNextStock
+
+  return [role, deliveryValueToNextRole]
+}
